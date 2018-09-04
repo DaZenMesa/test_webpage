c0c01c6760322040f1d70220199d9f322341c5ca
Mesa <mehufarm@gmail.com>
2018-09-04 11:12:47 -0700

    hdhdhdhdhdhdh

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("h1").click(function() {
-    $("p").css({"color":"red", "background-color":"blue"});
+    $("p").css({"color":"purple", "background-color":"coral"});
+	$("img").css({"border":"75px solid green"});
   } );
-} )
+} ) 
